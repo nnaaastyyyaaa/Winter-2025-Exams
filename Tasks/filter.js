6bd4e86ba13d323834adebfd926514fb1d08b439
Anastasia Andrienko <nnaaastyyyaaa06@gmail.com>
2024-12-29 13:58:31 +0200

    Remove unneeded operations and blocks

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -5,12 +5,10 @@
 const filter = (array, typeName) => {
   let result = [];
   for (element of array) {
-    const x = array.indexOf(C);
     if (typeof array[x] !== typeName) {
       remove.unshift(x);
     }
   }
-  for (const x of remove) array.splice(x, 1);
   return result;
 };
 
