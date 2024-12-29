86b3f44daa790f26f748c949b166d20693f94c81
Anastasia Andrienko <nnaaastyyyaaa06@gmail.com>
2024-12-29 14:47:21 +0200

    Replace String.prototype.substring() with String.prototype.replace()

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -6,14 +6,12 @@ const replace = (str, substr, newstr) => {
   if (substr === '') {
     return str;
   } else {
-    let res = '';
     const index = str.indexOf(substr);
     if (index === -1) {
-      return res + str;
+      return str;
     } else {
-      const start = str.substring(0, index);
-      const end = str.substring(index + substr.length, str.length);
-      return (res += start + newstr + end);
+      const result = str.replace(substr, newstr);
+      return result;
     }
   }
 };
