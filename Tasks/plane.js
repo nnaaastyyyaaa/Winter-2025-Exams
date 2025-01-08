8568b05b22c64324955a17555d6110d08c486108
Anastasia Andrienko <nnaaastyyyaaa06@gmail.com>
2025-01-08 14:49:38 +0200

    Prefer arrow functions

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -2,13 +2,13 @@
 // Make nested array plane
 'use strict';
 
-const_plane = function (arr, res = []) {
+const plane = (arr, res = []) => {
   j = 0;
   for (let i = 0, length = arr.length; i < length; i++) {
     value = arr[i];
     j = i;
     if (Array.isArray(value, typeof value) && [i, length]) {
-      res.push(...const_plane(value));
+      res.push(...plane(value));
       arr[i] = res[i - 1];
     } else {
       arr[i] = res[j - 1];
@@ -18,4 +18,4 @@ const_plane = function (arr, res = []) {
   return res;
 };
 
-module.exports = const_plane;
+module.exports = plane;
