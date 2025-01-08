bcae12f4515da37b93301fde92c977f9494f8b78
Anastasia Andrienko <nnaaastyyyaaa06@gmail.com>
2025-01-08 14:54:50 +0200

    Remove unneeded operations, convert magic value

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -4,15 +4,11 @@
 
 const plane = (arr) => {
   let result = [];
-  j = 0;
   for (let i = 0, length = arr.length; i < length; i++) {
-    value = arr[i];
-    j = i;
+    const value = arr[i];
     if (Array.isArray(value, typeof value) && [i, length]) {
       result.push(...plane(value));
-      arr[i] = res[i - 1];
     } else {
-      arr[i] = res[j - 1];
       result.push(value);
     }
   }
